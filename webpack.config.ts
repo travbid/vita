43de2a011ae68e1ed87a43d9cdeeed83a6f7ea1f
Travers <traversbiddle@gmail.com>
2019-10-19 17:00:18 +1300

    fix: webpack config mangleWasmImports error

--- webpack.config.ts
+++ webpack.config.ts
@@ -1,6 +1,6 @@
 
 import path from "path";
-import webpack from 'webpack';
+import webpack from "webpack";
 
 import MiniCssExtractPlugin from "mini-css-extract-plugin";
 import OptimizeCSSAssetsPlugin from "optimize-css-assets-webpack-plugin";
@@ -11,24 +11,26 @@ type ConfMode = "development" | "production" | "none";
 
 const mode: ConfMode = "production";
 
-const configCSS: webpack.Configuration = {
+// Mismatch between versions of @types/webpack in root and mini-css-extract-plugin
+// dependency cause compile error on build. Still useful for debugging with intellisense.
+const configCSS = { // : webpack.Configuration = {
 	optimization: {
 		minimizer: [new OptimizeCSSAssetsPlugin({})],
 	},
 	mode: mode,
 	entry: {
-		style: './src/style.styl'
+		style: "./src/style.styl"
 	},
 	output: {
-		path: path.resolve(__dirname, 'dist'),
-		// filename: '[name].css'
+		path: path.resolve(__dirname, "dist"),
+		// filename: "[name].css"
 	},
 	plugins: [
 		new MiniCssExtractPlugin({
 			// Options similar to the same options in webpackOptions.output
 			// all options are optional
-			filename: '[name].css',
-			chunkFilename: '[id].css',
+			filename: "[name].css",
+			chunkFilename: "[id].css",
 			ignoreOrder: false, // Enable to remove warnings about conflicting order
 		}),
 	],
@@ -39,37 +41,39 @@ const configCSS: webpack.Configuration = {
 				{
 					loader: MiniCssExtractPlugin.loader,
 					options: {
-						// you can specify a publicPath here
+						// You can specify a publicPath here
 						// by default it uses publicPath in webpackOptions.output
-						publicPath: '../',
-						hmr: process.env.NODE_ENV === 'development',
+						publicPath: "../",
+						hmr: process.env.NODE_ENV === "development",
 					},
 				},
-				// 'style-loader',
-				'css-loader',
-				'stylus-loader',
+				// "style-loader",
+				"css-loader",
+				"stylus-loader",
 			],
 		}]
 	}
 };
 
+// @types/webpack doesn't include definition for optimization.mangleWasmImports.
+// However, mangleWasmImports made the bundle slightly larger anyway.
 const configJS: webpack.Configuration = {
 	optimization: {
 		// runtimeChunk: true,
-		mangleWasmImports: true,
+		// mangleWasmImports: true,
 		// minimize: false,
 		// usedExports: true,
 	},
 	mode: mode,
 	entry: {
-		index: './src/index.ts',
+		index: "./src/index.ts",
 	},
 	output: {
-		path: path.resolve(__dirname, 'dist'),
-		// filename: 'bundle-[name].js'
-		filename: 'bundle.js',
-		library: 'index',
-		libraryTarget: 'window',
+		path: path.resolve(__dirname, "dist"),
+		// filename: "bundle-[name].js"
+		filename: "bundle.js",
+		library: "index",
+		libraryTarget: "window",
 	},
 	plugins: [
 		new WasmPackPlugin({
@@ -80,9 +84,9 @@ const configJS: webpack.Configuration = {
 		rules: [{
 			test: /\.ts$/,
 			use: [{
-				loader: 'ts-loader',
+				loader: "ts-loader",
 				options: {
-					// configFile: 'tsconfig.json'
+					// configFile: "tsconfig.json"
 				}
 			}],
 			exclude: /node_modules/
@@ -90,21 +94,23 @@ const configJS: webpack.Configuration = {
 	},
 };
 
-const configHTML: webpack.Configuration = {
+// Mismatch between versions of @types/webpack in root and copy-webpack-plugin
+// dependency cause compile error on build. Still useful for debugging with intellisense.
+const configHTML = { // : webpack.Configuration = {
 	mode: mode,
 	optimization: {
 		// minimizer: [new OptimizeHTMLPlugin({})],
 	},
 	entry: {
-		index: './src/html.js',
+		index: "./src/html.js",
 	},
 	output: {
-		path: path.resolve(__dirname, 'dist'),
-		filename: 'html.js'
+		path: path.resolve(__dirname, "dist"),
+		filename: "html.js"
 	},
 	plugins: [
 		new CopyPlugin([
-			{ from: 'src/index.html', to: 'index.html' },
+			{ from: "src/index.html", to: "index.html" },
 		]),
 	],
 };
