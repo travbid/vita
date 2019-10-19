e7146399ad61de86bde97aad90ec2d2cab8b163e
Travers <traversbiddle@gmail.com>
2019-10-19 14:20:35 +1300

    refactor: Use wasm for matrix rotation

--- src/matrix.ts
+++ src/matrix.ts
@@ -6,10 +6,12 @@
 // Add an async splitpoint (i. e. import()) somewhere between your entrypoint and the WebAssembly module"
 // Change your tsconfig to module: esnext
 // Or load the WASM module from a plain Javascript file (non-Typescript)
-let invertMat4x4: ((arg0: Float64Array) => void) | undefined = undefined;
+let invertMat4: ((arg0: Float64Array) => void) | undefined = undefined;
+let rotateMat4: ((arg0: Float64Array, arg1: number, arg2: Float64Array) => void) | undefined = undefined;
 const loadWasm = async (): Promise<void> => {
-	const {invert_mat4x4} = await import('../vita-wasm/pkg');
-	invertMat4x4 = invert_mat4x4;
+	const { invertMat4x4, rotateMat4x4 } = await import('../vita-wasm/pkg');
+	invertMat4 = invertMat4x4;
+	rotateMat4 = rotateMat4x4;
 	console.log(invertMat4x4);
 };
 loadWasm();
@@ -35,9 +37,8 @@ export class Mat4 {
 		return data;
 	}
 	invert(): void {
-		if (invertMat4x4 === undefined) { return; }
-		invertMat4x4(this.m)
-		return;
+		if (invertMat4 == undefined) { return; }
+		invertMat4(this.m)
 	}
 	perspective(fov: number, aspect: number, near: number, far: number): void {
 		const h = 2 * near * Math.tan(fov);
@@ -49,44 +50,9 @@ export class Mat4 {
 			0, 0, -1, 0,
 		]);
 	}
-	rotate(rad: number, axis: [number, number, number]): void {
-		const EPSILON = 0.00001;
-		let x = axis[0], y = axis[1], z = axis[2];
-		let len = Math.hypot(x, y, z);
-
-		if (len < EPSILON) { return; }
-
-		len = 1 / len;
-		x *= len;
-		y *= len;
-		z *= len;
-
-		const s = Math.sin(rad);
-		const c = Math.cos(rad);
-		const t = 1 - c;
-
-		// Construct the elements of the rotation matrix
-		const b00 = x * x * t + c, b01 = y * x * t + z * s, b02 = z * x * t - y * s;
-		const b10 = x * y * t - z * s, b11 = y * y * t + c, b12 = z * y * t + x * s;
-		const b20 = x * z * t + y * s, b21 = y * z * t - x * s, b22 = z * z * t + c;
-
-		const a00 = this.m[0], a01 = this.m[4], a02 = this.m[8], a03 = this.m[12];
-		const a10 = this.m[1], a11 = this.m[5], a12 = this.m[9], a13 = this.m[13];
-		const a20 = this.m[2], a21 = this.m[6], a22 = this.m[10], a23 = this.m[14];
-
-		// Perform rotation-specific matrix multiplication
-		this.m[0] = a00 * b00 + a10 * b01 + a20 * b02;
-		this.m[4] = a01 * b00 + a11 * b01 + a21 * b02;
-		this.m[8] = a02 * b00 + a12 * b01 + a22 * b02;
-		this.m[12] = a03 * b00 + a13 * b01 + a23 * b02;
-		this.m[1] = a00 * b10 + a10 * b11 + a20 * b12;
-		this.m[5] = a01 * b10 + a11 * b11 + a21 * b12;
-		this.m[9] = a02 * b10 + a12 * b11 + a22 * b12;
-		this.m[13] = a03 * b10 + a13 * b11 + a23 * b12;
-		this.m[2] = a00 * b20 + a10 * b21 + a20 * b22;
-		this.m[6] = a01 * b20 + a11 * b21 + a21 * b22;
-		this.m[10] = a02 * b20 + a12 * b21 + a22 * b22;
-		this.m[14] = a03 * b20 + a13 * b21 + a23 * b22;
+	rotate(rad: number, axis: number[]): void {
+		if (rotateMat4 === undefined) { return; }
+		rotateMat4(this.m, rad, new Float64Array(axis));
 	}
 	translate(x: number, y: number, z: number): void {
 		this.m[3] += x;
